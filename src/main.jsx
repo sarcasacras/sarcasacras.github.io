fa556d96f9bbe973142c97cf1f394775eb5a788c
Sergey <94703156+Ex10101@users.noreply.github.com>
2023-07-22 12:22:58 +0200

    start

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Root from './routes/Root.jsx'
+import ErrorPage from './routes/ErrorPage.jsx'
+import Contacts from './routes/Contacts.jsx'
+import Portfolio from './routes/Portfolio.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/contacts",
+    element: <Contacts />,
+  },
+  {
+    path: "/portfolio",
+    element: <Portfolio />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
