6ede3b826600a125ba5acc50d72fe9e876d3b525
Ex10101 <sergeydavidovich12@gmail.com>
2023-07-24 16:19:17 +0200

    3 section complete

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -9,11 +9,34 @@ export default function Root() {
             <Sidebar />
             <div className='main-section first section' id='section1'>
                 <img src="src/assets/Ellipse.png" alt="" />
-                <p>Hello, i am Andrei!</p>
+                <p>Hello, i am <span id='main-name'>Andrei</span></p>
             </div>
             <div className='main-section second section' id='section2'>
                 <img src="src/assets/Ellipse.png" alt="" />
-                <p>I am a passionate Full-Stack Developer</p>
+                <p>I am a passionate <span id='main-name'>Full-Stack Developer</span></p>
+            </div>
+            <div className='main-section third section' id='section3'>
+                <h2>My skills breakdown</h2>
+                <div className='skill-container'>
+                    <div className='skill-group'>
+                        <h3>Front-End:</h3>
+                        <ul className='skill-list'>
+                            <li>• HTML 5</li>
+                            <li>• CSS 3</li>
+                            <li>• JavaScript</li>
+                            <li>• React</li>
+                        </ul>
+                    </div>
+                    <div className='skill-group'>
+                        <h3>Back-End:</h3>
+                        <ul className='skill-list'>
+                            <li>• Node.js</li>
+                            <li>• Express.js</li>
+                            <li>• MongoDB</li>
+                            <li>• Mongoose</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </main>
     )
